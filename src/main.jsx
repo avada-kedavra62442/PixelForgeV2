ac90bf9a054ea31fe72621ee5332ace51bac7957
avada-kedavra62442 <aarav.krish62442@gmail.com>
2026-08-17 19:40:03 +0530

    Code

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import PixelForge from "./PixelForge_FIXED";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <PixelForge />
+    <App />
+    <Analytics />
   </React.StrictMode>
 );
