9a83309e85f2923060c7c6db8185ccd1e2a46727
avada-kedavra62442 <aarav.krish62442@gmail.com>
2026-08-17 17:11:48 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import PixelForge from "./PixelForge";
+import PixelForge from "./PixelForge_FIXED";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
